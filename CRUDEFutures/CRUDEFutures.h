52836915e3754521790992490d7a3b4d179393ac
Jason Welch <jwelch@groupon.com>
2016-06-02 12:57:03 -0400

    Initial Commit

---
+++ CRUDEFutures/CRUDEFutures.h
@@ -0,0 +1,19 @@
+//
+//  CRUDEFutures.h
+//  CRUDEFutures
+//
+//  Created by Jason Welch on 6/2/16.
+//  Copyright © 2016 Jason Welch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CRUDEFutures.
+FOUNDATION_EXPORT double CRUDEFuturesVersionNumber;
+
+//! Project version string for CRUDEFutures.
+FOUNDATION_EXPORT const unsigned char CRUDEFuturesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CRUDEFutures/PublicHeader.h>
+
+
