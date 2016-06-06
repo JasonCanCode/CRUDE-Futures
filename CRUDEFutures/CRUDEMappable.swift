e7cee69233f4aba6c2f0bea8821ee5079b2d9138
Jason Welch <jwelch@groupon.com>
2016-06-06 10:08:42 -0400

    CRUDEMappable added
    Use one protocol to rule them all
    CRUDEMappable does all the things

---
+++ CRUDEFutures/CRUDEMappable.swift
@@ -0,0 +1,9 @@
+//
+//  CRUDEMappable.swift
+//  CRUDEFutures
+//
+//  Created by Jason Welch on 6/6/16.
+//  Copyright © 2016 Jason Welch. All rights reserved.
+//
+
+public protocol CRUDEMappable: CRUDECreatable, CRUDEReadable, CRUDEUpdatable, CRUDEDeletable, CRUDEEnumeratable {}
